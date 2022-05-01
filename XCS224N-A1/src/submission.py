5ce46fbd5aa461af46b4e890f18daf65ce3ad479
Varun <40916057+Prais3@users.noreply.github.com>
2022-05-01 18:21:39 -0400

    Assignment 1

---
+++ XCS224N-A1/src/submission.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import numpy as np
+import scipy as sp
+import matplotlib
+import matplotlib.pyplot as plt
+from utils import *
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA
+
+sys.path.append(os.path.abspath(os.path.join('..')))
+
+def distinct_words(corpus):
+    """ Determine a list of distinct words for the corpus.
+        Params:
+            corpus (list of list of strings): corpus of documents
+        Return:
+            corpus_words (list of strings): list of distinct words across the corpus, sorted (using python 'sorted' function)
+            num_corpus_words (integer): number of distinct words across the corpus
+    """
+    corpus_words = []
+    num_corpus_words = 0
+
+    # ### START CODE HERE ###
+    corpus_words = [word for word_lst in corpus for word in word_lst if word not in corpus_words]
+
+    # Using set method to avoid duplicates
+    corpus_words = list(sorted(set((corpus_words))))
+    num_corpus_words = len(corpus_words)
+
+    # ### END CODE HERE ###
+
+    return corpus_words, num_corpus_words
+
+def compute_co_occurrence_matrix(corpus, window_size=4):
+    """ Compute co-occurrence matrix for the given corpus and window_size (default of 4).
+
+        Note: Each word in a document should be at the center of a window. Words near edges will have a smaller
+              number of co-occurring words.
+
+              For example, if we take the document "START All that glitters is not gold END" with window size of 4,
+              "All" will co-occur with "START", "that", "glitters", "is", and "not".
+
+        Params:
+            corpus (list of list of strings): corpus of documents
+            window_size (int): size of context window
+        Return:
+            M (numpy matrix of shape (number of unique words in the corpus , number of unique words in the corpus)):
+                Co-occurrence matrix of word counts.
+                The ordering of the words in the rows/columns should be the same as the ordering of the words given by the distinct_words function.
+            word2Ind (dict): dictionary that maps word to index (i.e. row/column number) for matrix M.
+    """
+    words, num_words = distinct_words(corpus)
+    M = None
+    word2Ind = {}
+
+    # ### START CODE HERE ###
+
+    # Simple loop to map the words to their index
+    for index in range(len(words)):
+        word2Ind[words[index]] = index
+
+    M = np.zeros((num_words, num_words))
+
+    for sent in corpus:
+        for center_index in range(len(sent)):
+            # calculating left and right side word indices
+            left_index = center_index - window_size
+            right_index = center_index + window_size + 1
+            for context_index in range(left_index, right_index):
+                # if not the center word - checking index
+                if context_index != center_index:
+                    # if within the sentence length
+                    if 0 <= context_index < len(sent):
+                        context_word = sent[context_index]
+                        center_word = sent[center_index]
+
+                        # Update the center and context word indices in matrix M
+                        context = word2Ind[context_word]
+                        center = word2Ind[center_word]
+                                                      
+                        M[center][context] += 1
+    # ### END CODE HERE ###
+
+    return M, word2Ind
+
+def reduce_to_k_dim(M, k=2):
+    """ Reduce a co-occurrence count matrix of dimensionality (num_corpus_words, num_corpus_words)
+        to a matrix of dimensionality (num_corpus_words, k) using the following SVD function from Scikit-Learn:
+            - http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.TruncatedSVD.html
+
+        Params:
+            M (numpy matrix of shape (number of unique words in the corpus , number of corpus words)): co-occurrence matrix of word counts
+            k (int): embedding size of each word after dimension reduction
+        Return:
+            M_reduced (numpy matrix of shape (number of corpus words, k)): matrix of k-dimensioal word embeddings.
+                    In terms of the SVD from math class, this actually returns U * S
+    """
+    np.random.seed(4355)
+    n_iters = 10     # Use this parameter in your call to `TruncatedSVD`
+    M_reduced = None
+    print("Running Truncated SVD over %i words..." % (M.shape[0]))
+
+    # ### START CODE HERE ###
+    M_reduced = TruncatedSVD(n_components=k, n_iter=n_iters).fit_transform(M)
+    # ### END CODE HERE ###
+
+    print("Done.")
+    return M_reduced
+
+def main():
+    #matplotlib.use('agg')
+    plt.rcParams['figure.figsize'] = [10, 5]
+
+    assert sys.version_info[0] == 3
+    assert sys.version_info[1] >= 5
+
+    def plot_embeddings(M_reduced, word2Ind, words, title):
+
+        for word in words:
+            idx = word2Ind[word]
+            x = M_reduced[idx, 0]
+            y = M_reduced[idx, 1]
+            plt.scatter(x, y, marker='x', color='red')
+            plt.text(x, y, word, fontsize=9)
+        plt.savefig(title)
+        plt.show()
+
+    #Read in the corpus
+    reuters_corpus = read_corpus()
+
+    M_co_occurrence, word2Ind_co_occurrence = compute_co_occurrence_matrix(reuters_corpus)
+    M_reduced_co_occurrence = reduce_to_k_dim(M_co_occurrence, k=2)
+    # Rescale (normalize) the rows to make them each of unit-length
+    M_lengths = np.linalg.norm(M_reduced_co_occurrence, axis=1)
+    M_normalized = M_reduced_co_occurrence / M_lengths[:, np.newaxis] # broadcasting
+
+    words = ['barrels', 'bpd', 'ecuador', 'energy', 'industry', 'kuwait', 'oil', 'output', 'petroleum', 'venezuela']
+    plot_embeddings(M_normalized, word2Ind_co_occurrence, words, 'co_occurrence_embeddings_(soln).png')
+
+if __name__ == "__main__":
+    main()
